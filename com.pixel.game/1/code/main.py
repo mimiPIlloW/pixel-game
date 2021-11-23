9225e7b8ced842427e950f95ac63168d5540334c
mimiPillow <erenstonxemin@gmail.com>
2021-11-23 11:49:32 +0300

    Adding a player, setting a level, adding a map.

--- com.pixel.game/1/code/main.py
+++ com.pixel.game/1/code/main.py
@@ -1,10 +1,11 @@
 import pygame, sys
+from settings import *
+from level import Level
 
 pygame.init()
-screen_width = 1200
-screen_height = 700
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map, screen)
 
 while True:
     for event in pygame.event.get():
@@ -13,7 +14,7 @@ while True:
             sys.exit()
 
     screen.fill('black')
+    level.run()
 
     pygame.display.update()
     clock.tick(60)
-
---
+++ com.pixel.game/1/code/settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                            ',
+'                            ',
+'X                           ',
+'    XX    XXX         XX    ',
+'                 XX         ',
+'              XX         XX ',
+'X   XX      XX              ',
+' XXX      XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'P   XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64
+screen_width = 1300
+screen_height = len(level_map) * tile_size
